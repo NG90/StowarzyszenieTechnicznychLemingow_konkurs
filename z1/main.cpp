486db53804c9de281d0347c23fdf07653a5ae217
kyrtaq <kyrtaq13@gmail.com>
2020-06-10 23:20:20 +0200

    added some ways to reverse a vector

---
+++ z1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "reverse_vector.hpp"
+
+template <typename T>
+void print(T vec) {
+    for (const auto el : vec) {
+        std::cout << el << ' ';
+    }
+}
+
+int main() {
+    std::vector<int> test{1, 2, 3, 4, 5};
+    reverseVector3(test);
+    print(test);
+    return 0;
+}
---
+++ z1/reverse_vector.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+void reverseVector1(std::vector<int>&);
+
+void reverseVector2(std::vector<int>&);
+
+void reverseVector3(std::vector<int>&);
+
+void reverseVector4(std::vector<int>&);
+
+void reverseVector5(std::vector<int>&);
+
+void reverseVector6(std::vector<int>&);
