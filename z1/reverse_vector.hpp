6eb61205b44a4936026fedc7af321fd4a85fc8e1
kozlofski <kozlofski.dev@gmail.com>
2020-06-11 16:54:31 +0200

    Unnecessary newlines between declarations removed

--- z1/reverse_vector.hpp
+++ z1/reverse_vector.hpp
@@ -3,21 +3,12 @@
 #include <vector>
 
 void reverseVector1(std::vector<int>&);
-
 void reverseVector2(std::vector<int>&);
-
 void reverseVector3(std::vector<int>&);
-
 void reverseVector4(std::vector<int>&);
-
 void reverseVector5(std::vector<int>&);
-
 void reverseVector6(std::vector<int>&);
-
 void reverseVector7(std::vector<int>&);
-
 void reverseVector8(std::vector<int>&);
-
 void reverseVector9(std::vector<int>&);
-
 void reverseVector10(std::vector<int>&);
